df79d5f807d9eeefe632dec6f7348bc6095c918c
saina <sainagarjuna184@gmail.com>
2023-10-04 22:59:03 +0530

    rre

---
+++ modules/alb/main.tf
@@ -0,0 +1,44 @@
+
+#need to create security group check in for resource in terraform registry
+resource "aws_security_group" "security_group" {
+  name        = "${var.env}-${var.alb_type}-sg"
+  description = "${var.env}-${var.alb_type}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.alb_sg_allow_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-${var.alb_type}-sg"
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "${var.env}-${var.alb_type}"
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group.id]
+  subnets            = var.subnets
+  tags = {
+    Environment = "${var.env}-${var.alb_type}"
+  }
+}
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = var.dns_name
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_lb.alb.dns_name]
+}
