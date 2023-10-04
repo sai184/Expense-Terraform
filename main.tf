c605ccaf477a9cc16df69b11a986bac113788432
saina <sainagarjuna184@gmail.com>
2023-10-04 21:29:39 +0530

    rre

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 module "vpc" {
-  source = "./env-dev"
+  source = "./modules/vpc"
   vpc_cidr      = var.vpc_cidr
+  env = var.env
   public_subnets = var.public_subnets
   private_subnets = var.private_subnets
   azs = var.azs
   default_vpc_id = var.default_vpc_id
   default_vpc_id_cidr = var.default_vpc_id_cidr
   default_route_table_id = var.default_route_table_id
+  account_id = var.account_id
 }
