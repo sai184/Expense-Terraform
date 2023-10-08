06e1d47db03cf2e45729a84c9bcc10d7f56ce80d
saina <sainagarjuna184@gmail.com>
2023-10-08 15:59:38 +0530

    rre

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "security_group" {
 
 resource "aws_lb" "alb" {
   name               = "${var.env}-${var.alb_type}"
-  internal           = var.internal
+  internal           = var.internal                           #
   load_balancer_type = "application"
   security_groups    = [aws_security_group.security_group.id]
   subnets            = var.subnets
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -34,7 +34,7 @@ resource "aws_db_subnet_group" "main" {                   #we need to create whi
 
 
 
-resource "aws_rds_cluster" "main" {
+resource "aws_rds_cluster" "main" {                                     #cluster is outbox it is like vpc virtual entry which is an virtual entity inside this need to create some instances
   cluster_identifier     = "${var.env}-${var.component}"
   engine                 = "aurora-mysql"
   engine_version         = "5.7.mysql_aurora.2.11.3"
@@ -44,4 +44,13 @@ resource "aws_rds_cluster" "main" {
   master_password        = data.aws_ssm_parameter.master_password.value
   vpc_security_group_ids = [aws_security_group.main.id]
   skip_final_snapshot    = true
+}
+
+resource "aws_rds_cluster_instance" "main" {
+  count              = 1
+  identifier         = "${var.env}-${var.component}-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.main.id
+  instance_class     = var.instance_class            #how many insatances did you want inside clister group
+  engine             = aws_rds_cluster.main.engine
+  engine_version     = aws_rds_cluster.main.engine_version
 }
